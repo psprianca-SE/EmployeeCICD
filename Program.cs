50cf312ecca82a1160ab4feb5dadd57c221882cf
psprianca-SE <psprianca16@gmail.com>
2026-05-27 14:04:19 +0530

    changes

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freeze", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
 app.MapGet("/weatherforecast", () =>
