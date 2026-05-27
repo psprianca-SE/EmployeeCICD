12fe66fc1d650228230db57dcd8e8afdaf108e87
psprianca-SE <psprianca16@gmail.com>
2026-05-27 13:45:00 +0530

    changes

--- obj/Debug/net8.0/EmployeeCICD.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeCICD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeCICD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b70b09ed9df3925ede650bbf2e9318b2498aaa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba843d77df17288b74b942770a2c2dbb6381f194")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeCICD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeCICD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
